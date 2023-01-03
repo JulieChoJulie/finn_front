9fa5d502b513a82cef671fb5ebf2c9fbd255e442
JulieChoJulie <h32cho@gmail.com>
2023-01-03 14:24:47 -0800

    add login and signup pages

--- components/graph/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Graph = () => {
-  return <div>e</div>;
-};
-
-export default Graph;
--- layouts/App/index.tsx
+++ layouts/App/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Login from '@pages/Login';
+import Signup from '@pages/Signup';
+import Home from '@pages/Home';
 
 const App = () => {
-  return <div>Initial setting</div>;
+  return (
+  <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 };
 
 export default App;
